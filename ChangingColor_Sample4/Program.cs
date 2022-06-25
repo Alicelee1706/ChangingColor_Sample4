4f8e3897a0f3c61489df8d2bae6c025ba8213dab
Alicelee1706 <78105672+Alicelee1706@users.noreply.github.com>
2022-06-25 16:49:09 +0700

    Update Program.cs

--- ChangingColor_Sample4/Program.cs
+++ ChangingColor_Sample4/Program.cs
@@ -57,24 +57,103 @@ class HelloWorld
                     }
                     break;
             }
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.SetCursorPosition(xOld, yOld);
-            Console.WriteLine("Hello World");
+        }
+        while (true);
+    }
+}
+class Cell
+{
+    private int[,] cell;
+    private int x, y;
+    private ConsoleColor bg, fg;
+    private int[,] NextCell()
+    {
+        int maxCell = 6;
+        Random rnd = new Random();
+        return GetCell(rnd.Next(maxCell) + 1);
+    }
+    private int[,] GetCell(int idx)
+    {
 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine("Hello World");
-
-            Console.SetCursorPosition(0, 20);
-            Console.WriteLine("_______________________________|");
-
-            for (int i = 0; i < 20; i++)
+    }
+    public void Draw ()
+    {
+        int x1 = x, y1 = y;
+        Console.ForegroundColor = fg;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
             {
-                Console.SetCursorPosition(31, i);
-                Console.WriteLine("|");
-
+                Console.SetCursorPosition(x1 + i, y1);
+                Console.Write("{0}", b1[i,j]);
+            }    
+            y1++;
+        }
+    }
+    private void ClearT()
+    {
+        int x1 = xOld, y1 = yOld;
+        Console.ForegroundColor = bg;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.SetCursorPosition(x1 + i, y1);
+                Console.Write("{0}", b1[i,j]);
+            }    
+            y1++;
+        }
+    }
+    public void Move ()
+    {
+        do
+        {
+            xOld = x;
+            yOld = y;
+            ConsoleKeyInfo ke;
+            ke = Console.ReadKey();
+            if (ke.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+            switch (ke.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    if (x > 0)
+                    {
+                        x--;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (x < 19)
+                    {
+                        x++;
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (y > 0)
+                    {
+                        y--;
+                    }
+       
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (y < 19)
+                    {
+                        y++;
+                    }
+                    break;
             }
         }
         while (true);
+    }
+    public Cell (ConsoleColor bg, ConsoleColor fg)
+    {
+        Cell = new int[5,3];
+        x = 0;
+        y = 0;
+        this bg = bg;
+        this fg = fg;
+        cell = NextCell();
     }
 }
