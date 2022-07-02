6d25c6a5e1f5392c73867b1e66f58d529c98c449
Alicelee1706 <78105672+Alicelee1706@users.noreply.github.com>
2022-07-02 16:28:25 +0700

    Update Program.cs

--- ChangingColor_Sample4/Program.cs
+++ ChangingColor_Sample4/Program.cs
@@ -15,7 +15,7 @@ class HelloWorld
 
         int x = 0;
         int y = 1;
-        
+
 
         var rkey = new Task(ReadKeys);
         var animate = new Task(Animation);
@@ -23,7 +23,6 @@ class HelloWorld
         animate.Start();
         var work = new[] { rkey };
         Task.WaitAll(work);
-
 
     }
     private static void ReadKeys()
@@ -49,13 +48,20 @@ class HelloWorld
                 case ConsoleKey.UpArrow:
                     if (cell.Y > 0)
                     {
-                        cell.Y = cell.Y  - 1;
+                        cell.Y = cell.Y - 1;
                     }
                     break;
                 case ConsoleKey.DownArrow:
                     if (cell.Y < 12)
                     {
                         cell.Y = cell.Y + 3;
+                        cell.rotate();
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (cell.Y < 30)
+                    {
+                        cell.Y = cell.Y + 1;
                     }
                     break;
             }
@@ -63,177 +69,160 @@ class HelloWorld
     }
     private static void Animation()
     {
-        for ( ;cell.Y < 12; )
+        for (; cell.Y < 12;)
         {
             Thread.Sleep(1000);
             cell.Y = cell.Y + 1;
         }
     }
-}    
+}
 class Cell
+{
+    private int[,] cell;
+    private int x, y;
+    private ConsoleColor bg, fg;
+    public void rotate()
     {
-        private int[,] cell;
-        private int x, y;
-        private ConsoleColor bg, fg;
-        private int[,] NextCell()
+        int[,] tmp = new int [3,3];
+        const int N = 3;
+        for (int i = 0; i < N; i++)
         {
-            int maxCell = 6;
-            Random rnd = new Random();
-            return GetCell(rnd.Next(maxCell) + 1);
+            for (int j = 0; j < N; j++)
+            {
+                tmp[j, 2 - i] = cell[i, j];
+            }
         }
-        private int[,] GetCell(int idx)
-        {
-            ArrayList DCell = new ArrayList();
-            DCell.Add
-                (new int[,]
-                {
+        cell = tmp;
+    }
+    private int[,] NextCell()
+    {
+        int maxCell = 6;
+        Random rnd = new Random();
+        return GetCell(rnd.Next(maxCell));
+        //return GetCell(1);
+    }
+    private int[,] GetCell(int idx)
+    {
+        ArrayList DCell = new ArrayList();
+        DCell.Add
+            (new int[,]
+            {
                 {1,1,1},
                 {1,1,1},
                 {1,1,1}
-                });
-            DCell.Add
-                (new int[,]
-                {
+            });
+        DCell.Add
+            (new int[,]
+            {
                 {1,0,0},
                 {1,0,0},
                 {1,1,1}
-                });
-            DCell.Add
-                (new int[,]
-                {
+            });
+        DCell.Add
+            (new int[,]
+            {
                 {1,1,0},
                 {0,1,0},
                 {0,1,1}
-                });
-            DCell.Add
-                (new int[,]
-                {
+            });
+        DCell.Add
+            (new int[,]
+            {
                 {1,1,1},
                 {0,1,0},
                 {0,0,0}
-                });
-            DCell.Add
-                (new int[,]
-                {
+            });
+        DCell.Add
+            (new int[,]
+            {
                 {0,0,0},
                 {0,1,0},
                 {0,0,0}
-                });
-            DCell.Add
-                (new int[,]
-                {
+            });
+        DCell.Add
+            (new int[,]
+            {
                 {0,1,0},
                 {0,1,0},
                 {0,1,0}
-                });
+            });
 
-            return (int[,])DCell[idx];
-        }
-        public void Draw()
+        return (int[,])DCell[idx];
+    }
+    public void Draw()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        for (int i = 0; i < 15; i++)
         {
-            Console.ForegroundColor = ConsoleColor.White;
-            for (int i = 0; i < 15; i++)
-            {
-                Console.SetCursorPosition(15, i);
-                Console.WriteLine("|");
-            }
-            for (int i = 0; i < 15; i++)
-            {
-                Console.SetCursorPosition(i, 15);
-                Console.Write("-");
-            }
+            Console.SetCursorPosition(15, i);
+            Console.WriteLine("|");
+        }
+        for (int i = 0; i < 15; i++)
+        {
+            Console.SetCursorPosition(i, 15);
+            Console.Write("-");
+        }
         int y1 = y;
-            Console.ForegroundColor = fg;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.SetCursorPosition(x + j, y1+i);
-                    Console.Write("{0}", cell[i, j] == 0 ? ' ' : '*');
-                }
-            }
-        }
-        public void Clean()
+        Console.ForegroundColor = fg;
+        for (int i = 0; i < 3; i++)
         {
-            int y1 = y;
-            Console.ForegroundColor = bg;
-            for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.SetCursorPosition(x + j, y1+i);
-                    Console.Write("{0}", cell[i, j] == 0 ? ' ' : '*');
-                }
-            }
-        }
-        public void Move()
-        {
-            do
-            {
-                ConsoleKeyInfo ke;
-                ke = Console.ReadKey();
-                if (ke.Key == ConsoleKey.Escape)
-                {
-                    break;
-                }
-                switch (ke.Key)
-                {
-                    case ConsoleKey.LeftArrow:
-                        if (x > 0)
-                        {
-                            x--;
-                        }
-                        break;
-                    case ConsoleKey.RightArrow:
-                        x++;
-                        break;
-                    case ConsoleKey.UpArrow:
-                        if (y > 0)
-                        {
-                            y--;
-                        }
-
-                        break;
-                    case ConsoleKey.DownArrow:
-                        y++;
-                        break;
-                }
-            }
-            while (true);
-        }
-        public Cell(ConsoleColor bg, ConsoleColor fg)
-        {
-            cell = new int[5, 3];
-            x = 0;
-            y = 0;
-            this.bg = bg;
-            this.fg = fg;
-            cell = NextCell();
-        }
-        public int X
-        {
-            set
-            {
-                Clean();
-                x = value;
-                Draw();
-        }
-            get
-            {
-                return x;
-            }
-        }
-        public int Y
-        {
-            set
-            {
-                Clean();
-                y = value;
-                Draw();
-            }
-            get
-            {
-                return y;
+                Console.SetCursorPosition(x + j, y1 + i);
+                Console.Write("{0}", cell[i, j] == 0 ? ' ' : '*');
             }
         }
     }
+    public void Clean()
+    {
+        int y1 = y;
+        Console.ForegroundColor = bg;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.SetCursorPosition(x + j, y1 + i);
+                Console.Write("{0}", cell[i, j] == 0 ? ' ' : '*');
+            }
+        }
+    }
+    public Cell(ConsoleColor bg, ConsoleColor fg)
+    {
+        cell = new int[5, 3];
+        x = 0;
+        y = 0;
+        this.bg = bg;
+        this.fg = fg;
+        cell = NextCell();
+    }
+    public int X
+    {
+        set
+        {
+            Clean();
+            x = value;
+            Draw();
+        }
+        get
+        {
+            return x;
+        }
+    }
+    public int Y
+    {
+        set
+        {
+            Clean();
+            y = value;
+            Draw();
+        }
+        get
+        {
+            return y;
+        }
+    }
+}
+class Board
+{
+    private int table;
+}
